27edef569bb0e9acfb66f0b23938e8148f1be9ec
Nikhil Garg <nikhilgarg855@gmail.com>
2023-10-24 18:58:03 +0530

    updating definitions

--- SoapUI/src/test/java/stepDefinitions/AddPlace.java
+++ SoapUI/src/test/java/stepDefinitions/AddPlace.java
@@ -3,28 +3,59 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import io.restassured.response.Response;
+
+import org.hamcrest.Matchers.*;
+
 
 public class AddPlace {
 
+	String payload,baseURI="https://rahulshettyacademy.com/maps/api/place/add/json?key=qaclick123";
 
 	@Given("Add place payload")
 	public void add_place_payload() {
-	    // Write code here that turns the phrase above into concrete actions
-	    System.out.println("In given");
+	   
+	payload= "{\r\n"
+			+ "  \"location\": {\r\n"
+			+ "    \"lat\": -38.383494,\r\n"
+			+ "    \"lng\": 33.427362\r\n"
+			+ "  },\r\n"
+			+ "  \"accuracy\": 50,\r\n"
+			+ "  \"name\": \"Frontline house\",\r\n"
+			+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
+			+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
+			+ "  \"types\": [\r\n"
+			+ "    \"shoe park\",\r\n"
+			+ "    \"shop\"\r\n"
+			+ "  ],\r\n"
+			+ "  \"website\": \"http://google.com\",\r\n"
+			+ "  \"language\": \"French-IN\"\r\n"
+			+ "}\r\n"
+			+ "";
 	}
+	
 	@When("User calls {string} with {string} request")
-	public void user_calls_with_request(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-		 System.out.println("In when");
+	public void user_calls_with_request(String string1, String string2) {
+	   
+		string1="addPlace";
+		string2 = "POST";
+		
+		Response response = given().body(payload).when().post(baseURI);
+		System.out.println("response: "+response.asString());
 	}
+	
 	@Then("API returns success code {string}")
 	public void api_returns_success_code(String string) {
-	    // Write code here that turns the phrase above into concrete actions
+	   
 		 System.out.println("In then1");
 	}
+	
 	@Then("{string} in response is {string}")
 	public void in_response_is(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
+	   
 		 System.out.println("In then2");
 	}
 
